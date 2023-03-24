c4517858afc44c7b2a0ebcca5da8420df5e53f49
Raoul Borges <paercebal@paercebal.org>
2023-03-25 00:25:33 +0100

    Add calendar-by-origin + misc. fixes

---
+++ HtmlKrynnEphemeris.calendars.js
@@ -0,0 +1,55 @@
+const g_calendar_id_default = "Default";
+const g_calendar_id_ergoth = "Ergoth";
+const g_calendar_id_plainsmen = "Plainsmen";
+const g_calendar_id_solamnic = "Solamnic";
+const g_calendar_id_dwarven = "Dwarven";
+const g_calendar_id_elven = "Elven";
+const g_calendar_id_kender = "Kender";
+const g_calendar_id_goblin = "Goblin";
+const g_calendar_id_god_days = "God-days";
+
+const g_calendar_id_list = [g_calendar_id_default, g_calendar_id_ergoth, g_calendar_id_plainsmen, g_calendar_id_solamnic, g_calendar_id_dwarven, g_calendar_id_elven, g_calendar_id_kender, g_calendar_id_goblin,  g_calendar_id_god_days];
+
+const g_weekdays_default = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const g_weekdays_ergoth = ["Gileadai", "Luindai", "Nuindai", "Soldai", "Manthus", "Shinarai", "Boreadai"];
+const g_weekdays_plainsmen = ["Friend Day", "Hunt Day", "Share Day", "Gather Day", "Barter Day", "Worship Day", "Council Day"];
+const g_weekdays_solamnic = ["Linaras", "Palast", "Majetag", "Kirinor", "Misham", "Bakukal", "Bracha"];
+const g_weekdays_dwarven = ["Brenzik", "Mithrik", "Adamachtis", "Aurachil", "Cuprig", "Ferramis", "Agorin"];
+const g_weekdays_elven = ["Gateway", "Bright Eye", "Night’s Eye", "Dead Eye", "Dream Dance", "Winged Trade", "World Tree"];
+const g_weekdays_kender = ["Praise Day", "Light Day", "Fire Day", "Winds Day", "Waters Day", "Ground Day", "Shadow Day"];
+const g_weekdays_goblin = ["Guard", "Pain", "Fear", "Rout", "Strife", "Feint", "Flank"];
+const g_weekdays_god_days = ["Gilean", "Lunitari", "Nuitari", "Solinari", "Majere", "Shinare", "Zivilyn"];
+
+const g_weekdays_map = {
+   "Default"   : g_weekdays_default,
+   "Ergoth"    : g_weekdays_ergoth,
+   "Plainsmen" : g_weekdays_plainsmen,
+   "Solamnic"  : g_weekdays_solamnic,
+   "Dwarven"   : g_weekdays_dwarven,
+   "Elven"     : g_weekdays_elven,
+   "Kender"    : g_weekdays_kender,
+   "Goblin"    : g_weekdays_goblin,
+   "God-days"  : g_weekdays_god_days 
+};
+
+const g_months_default = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const g_months_ergoth = ["Aelmont", "Rannmont", "Mishamont", "Chislmont", "Bran", "Corij", "Argon", "Sirrimont", "Reorxmont", "Hiddumont", "H’rarmont", "Phoenix"];
+const g_months_plainsmen = ["Ice Glaze", "Snow Deep", "Mountain Thaw", "Earth Wakes", "Flower Blooms", "Home Earth", "Raging Fire", "Dying Ember", "Harvest Home", "Leaf Gild", "Dark Cold", "Frost Eve"];
+const g_months_solamnic = ["Newkolt", "Deepkolt", "Brookgreen", "Yurthgreen", "Fleurgreen", "Holmswelth", "Fierswelt", "Paleswelt", "Reapember", "Gildember", "Darkember", "Frostkolt"];
+const g_months_dwarven = ["Dark-Crypt", "Dark-Deep", "Damp-Mood", "Damp-Chisel", "Dry-Anvil", "Dry-Axe", "Dry-Heat", "Dry-Forge", "Cold-Hammer", "Cold-Steel", "Cold-Rust", "Cold-Lode"];
+const g_months_elven = ["Winter Night", "Winter Deep", "Spring Dawning", "Spring Rain", "Spring Blossom", "Summer Home", "Summer Run", "Summer End", "Autumn Harvest", "Autumn Twilight", "Autumn Dark", "Winter Come"];
+const g_months_kender = ["Snowfun", "Darktime", "Windsong", "Raindrum", "Flowerfield", "Homefriends", "Wandertime", "Summerlaze", "Harvestfete", "Leafplay", "Bleakcold", "Blessings"];
+const g_months_goblin = ["Famine", "Madness", "Girding", "Scouting", "Campaign", "Siege", "Flame", "Sack", "Loot", "Betrayal", "Pestilence", "Reward"];
+const g_months_god_days = ["Chemosh", "Zeboim", "Mishakal", "Chislev", "Branchala", "Kiri-Jolith", "Sargonnas", "Sirrion", "Reorx", "Hiddukel", "Morgion", "Habbakuk"];
+
+const g_months_map = {
+   "Default"   : g_months_default,
+   "Ergoth"    : g_months_ergoth,
+   "Plainsmen" : g_months_plainsmen,
+   "Solamnic"  : g_months_solamnic,
+   "Dwarven"   : g_months_dwarven,
+   "Elven"     : g_months_elven,
+   "Kender"    : g_months_kender,
+   "Goblin"    : g_months_goblin,
+   "God-days"  : g_months_god_days 
+};
